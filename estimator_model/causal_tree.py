de1f751794c76d890feeaf4a7d30e9b74b80dbd2
bochenL <bochen.lv@gmail.com>
2022-03-24 12:10:33 +0800

    Add estimate of causal tree.

--- estimator_model/causal_tree.py
+++ estimator_model/causal_tree.py
@@ -1,12 +1,6 @@
-import numbers
-from estimator_model.utils import sample
-
 import numpy as np
 
-from abc import ABCMeta
-from abc import abstractclassmethod
-from math import ceil
-
+from sklearn.utils.validation import check_is_fitted
 from sklearn.tree._splitter import BestSplitter
 from sklearn.tree._tree import DepthFirstTreeBuilder, Tree
 
@@ -14,6 +8,32 @@ from .tree_criterion import CMSE
 
 
 class CausalTree:
+    # TODO: add confidence interval
+    """
+    A class for estimating causal effect with decision tree.
+
+    Attributes
+    ----------
+    feature_importances_ : ndarray of shape (n_features,)
+        The feature importances.
+    tree_ : Tree instance
+        The underlying Tree object.
+    max_depth : int, default to None
+    min_samples_split : int or float, default to 2
+    min_samples_leaf : int or float, default to 1
+    random_state : int
+    max_leaf_nodes : int, default to None
+    min_impurity_decrease : float, default to 0.0
+    ccp_alpha : non-negative float, default to 0.0
+
+    Methods
+    ----------
+    fit()
+    predict()
+    estimate()
+    apply()
+    """
+
     def __init__(
         self,
         max_depth=None,
@@ -24,6 +44,32 @@ class CausalTree:
         min_impurity_decrease=0.0,
         ccp_alpha=0.0,
     ):
+        """
+        Many parameters are similar to those of BaseDecisionTree of sklearn.
+
+        Parameters
+        ----------
+        max_depth : int, default to None
+            The maximum depth of the tree.
+        min_samples_split : int or float, default to 2
+            The minimum number of samples required to split an internal node.
+        min_samples_leaf : int or float, default to 1
+            The minimum number of samples required to be at a leaf node.
+        random_state : int
+        max_leaf_nodes : int, default to None
+            Grow a tree with ``max_leaf_nodes`` in best-first fashion.
+            Best nodes are defined as relative reduction in impurity.
+            If None then unlimited number of leaf nodes.
+        min_impurity_decrease : float, default to 0.0
+            A node will be split if this split induces a decrease of the impurity
+            greater than or equal to this value.
+        ccp_alpha : non-negative float, default to 0.0
+            Value for pruning the tree. #TODO: not implemented yet.
+
+        See Also
+        --------
+        BaseDecisionTree : The default implementation of decision tree.
+        """
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.min_samples_leaf = min_samples_leaf
@@ -34,6 +80,23 @@ class CausalTree:
         self.tree_ = None
 
     def fit(self, data, outcome, treatment, treatment_value=1):
+        """Fit the model to data.
+
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            Currently only support this. #TODO: try to support ndarray.
+        outcome : str or list of str
+            Name of the outcome.
+        treatment : str or list of str
+            Name of the treatment vector.
+        treatment_value : int, optional, defaults to 1
+            If treatment == treatment_value for a data point, then it is in
+            the treatment group, otherwise it is in the control group.
+
+        Returns:
+            self: The fitted causal tree.
+        """
         # TODO: currently only support binary treatment, try to find if we can
         # support discrete treatment.
         # Determine output settings
@@ -47,9 +110,8 @@ class CausalTree:
         n_outputs = 1
 
         # Check parameters and etermine output settings
-        sample_weight = (t == treatment).astype(int)
+        sample_weight = (t == treatment_value).astype(int)
         min_weight_leaf = 2  # maybe this needs more modifications
-        max_depth = 1
 
         # Build tree step 1. Set up criterion
         criterion = CMSE(1, n_samples)
@@ -77,21 +139,41 @@ class CausalTree:
             self.min_samples_split,
             self.min_samples_leafmin_samples_leaf,
             min_weight_leaf,
-            max_depth,
+            self.max_depth,
             self.min_impurity_decrease,
         )
         builder.build(self.tree_, X, y, sample_weight)
 
         return self
 
-    def predict(self):
-        pass
+    def predict(self, data, outcome=None, treatment=None):
+        if treatment is not None:
+            data = data.drop(treatment, axis=1)
+        if outcome is not None:
+            data = data.drop(outcome, axis=1)
 
-    def estimate(self):
-        pass
+        check_is_fitted(self)
+        proba = self.tree_.predict(data)
+        return proba[:, 0]
+
+    def estimate(self, data, outcome, treatment, treatment_value=1):
+        """Based on my current understanding, the causal tree only solves
+        estimation of heterogeneous causal effects. Thus we may not need
+        estimations for other effects.
+        """
+        self.fit(data, outcome, treatment, treatment_value)
+        result = self.predict(data, outcome, treatment)
+        return result
 
     def _prune_tree(self):
         pass
 
-    def apply(self):
+    def apply(self, X):
+        """Return the index of the leaf that each sample is predicted as.
+        """
+        check_is_fitted(self)
+        return self.tree_.apply(X)
+
+    @property
+    def feature_importance(self):
         pass
