c6de2ec8ed75988c98eda0df33d1f117696b6c72
lixfz <lixf@sovon.net>
2022-04-26 17:47:43 +0800

    Update estimators

---
+++ setup.py
@@ -0,0 +1,18 @@
+import numpy
+from Cython.Build import cythonize
+from setuptools import setup, Extension
+
+extensions = [
+    Extension('estimator_model.tree_criterion',
+              sources=[f'estimator_model/tree_criterion.pyx'],
+              include_dirs=[numpy.get_include()]),
+]
+
+setup(
+    name='ylearn-ext',
+    ext_modules=cythonize(extensions),
+    zip_safe=False,
+)
+
+# setup extensions:
+#     python setup.py build_ext --inplace
