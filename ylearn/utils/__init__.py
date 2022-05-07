1db956e2ca80cedf9dfae958f8dcb1e55a8d98cc
lixfz <lixf@sovon.net>
2022-05-07 11:33:33 +0800

    Update utilities

--- ylearn/utils/__init__.py
+++ ylearn/utils/__init__.py
@@ -1,14 +1,16 @@
-import pandas as pd
+import pandas as _pd
 
 
 def to_df(**data):
     dfs = []
     for k, v in data.items():
-        if len(v.shape) == 1:
-            dfs.append(pd.Series(v, name=k))
+        if v is None:
+            pass  # ignore the item
+        elif len(v.shape) == 1:
+            dfs.append(_pd.Series(v, name=k))
         elif v.shape[1] == 1:
-            dfs.append(pd.DataFrame(v, columns=[k]))
+            dfs.append(_pd.DataFrame(v, columns=[k]))
         else:
-            dfs.append(pd.DataFrame(v, columns=[f'{k}_{i}' for i in range(v.shape[1])]))
-    df = pd.concat(dfs, axis=1)
+            dfs.append(_pd.DataFrame(v, columns=[f'{k}_{i}' for i in range(v.shape[1])]))
+    df = _pd.concat(dfs, axis=1)
     return df
