c1b47ca023258737865ed05b0462a4e43044fd7d
lixfz <lixf@sovon.net>
2022-05-07 12:08:38 +0800

    Update test cases

--- tests/metaleaner_test.py
+++ tests/metaleaner_test.py
@@ -11,6 +11,11 @@ from ._common import validate_leaner
 
 _test_settings = {
     # data_generator: model
+    _dgp.generate_data_x1b_y1: GradientBoostingRegressor(),
+    _dgp.generate_data_x2b_y1: GradientBoostingRegressor(),
+    _dgp.generate_data_x1b_y2: LinearRegression(),
+    _dgp.generate_data_x2b_y2: MultiTaskLasso(),
+    _dgp.generate_data_x1m_y1: GradientBoostingRegressor(),
     _dgp.generate_data_x1b_y1_w5v0: GradientBoostingRegressor(),
     _dgp.generate_data_x2b_y1_w5v0: GradientBoostingRegressor(),
     _dgp.generate_data_x1b_y2_w5v0: LinearRegression(),
@@ -20,7 +25,9 @@ _test_settings = {
 
 
 @pytest.mark.parametrize('dg,combined',
-                         product(_test_settings.keys() - [_dgp.generate_data_x2b_y2_w5v0, ], [True, False]))
+                         product(_test_settings.keys() - [_dgp.generate_data_x2b_y2,
+                                                          _dgp.generate_data_x2b_y2_w5v0],
+                                 [True, False]))
 def test_sleaner(dg, combined):
     model = _test_settings[dg]
     validate_leaner(dg, SLearner(model=clone(model)), fit_kwargs=dict(combined_treatment=combined))
