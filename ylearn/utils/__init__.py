d8dc381451ff9be96b2e5d6ad0f0baaf1fc5215d
lixfz <lixf@sovon.net>
2022-05-25 21:08:21 +0800

    Add utility `Version`

--- ylearn/utils/__init__.py
+++ ylearn/utils/__init__.py
@@ -1,3 +1,8 @@
+try:
+    from packaging.version import Version
+except ModuleNotFoundError:
+    from distutils.version import LooseVersion as Version
+
 from ._common import const, infer_task_type, unique, to_repr
 from ._common import to_df, context, is_notebook, view_pydot
 from ._common import to_snake_case, to_camel_case, discard_none
