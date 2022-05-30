5f00a29b75d615b600b41614488ae6ef38eb6fdf
lixfz <lixf@sovon.net>
2022-05-30 19:44:52 +0800

    Update console

--- tests/console_test.py
+++ tests/console_test.py
@@ -31,3 +31,21 @@ def test_identify_treatment():
     cc.fit(data, outcome[0], treatment=None, adjustment=adjustment, covariate=covariate)
 
     _validate_it(cc, test_data)
+
+
+def test_discovery_treatment():
+    data, test_data, outcome, treatment, adjustment, covariate = _dgp.generate_data_x2b_y1()
+    cc = CausalConsole(identify_method='discovery')
+    # cc.fit(data, outcome[0], treatment=treatment, adjustment=adjustment, covariate=covariate)
+    cc.fit(data, outcome[0], treatment=None, adjustment=adjustment, covariate=covariate)
+
+    _validate_it(cc, test_data)
+
+
+def test_discovery_taci():
+    data, test_data, outcome, treatment, adjustment, covariate = _dgp.generate_data_x2b_y1()
+    cc = CausalConsole(identify_method='discovery')
+    # cc.fit(data, outcome[0], treatment=treatment, adjustment=adjustment, covariate=covariate)
+    cc.fit(data, outcome[0])
+
+    _validate_it(cc, test_data)
