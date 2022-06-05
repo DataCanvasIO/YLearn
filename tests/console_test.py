02e232bb4d63b78e9d65cf56ca7e838d40869980
lixfz <lixf@sovon.net>
2022-06-05 13:33:14 +0800

    Update test cases

--- tests/console_test.py
+++ tests/console_test.py
@@ -15,6 +15,10 @@ def _validate_it(cc, test_data):
     e = cc.local_causal_effect(test_data)
     print('local causal effect:', e, sep='\n')
 
+    if cc.scorers_ is not None:
+        score = cc.score()
+        print("score:", score)
+
 
 def test_basis():
     data, test_data, outcome, treatment, adjustment, covariate = _dgp.generate_data_x2b_y1()
@@ -47,5 +51,13 @@ def test_discovery_taci():
     cc = CausalConsole(identify_method='discovery')
     # cc.fit(data, outcome[0], treatment=treatment, adjustment=adjustment, covariate=covariate)
     cc.fit(data, outcome[0])
+
+    _validate_it(cc, test_data)
+
+
+def test_score():
+    data, test_data, outcome, treatment, adjustment, covariate = _dgp.generate_data_x2b_y1()
+    cc = CausalConsole(scorer='auto')
+    cc.fit(data, outcome[0], treatment=treatment, adjustment=adjustment, covariate=covariate)
 
     _validate_it(cc, test_data)
