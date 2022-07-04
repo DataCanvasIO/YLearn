c05debf0991242811e9c145e1908568508841773
bochenL <bochen.lv@gmail.com>
2022-07-04 14:34:46 +0800

    fix doc

--- docs/zh_CN/source/conf.py
+++ docs/zh_CN/source/conf.py
@@ -17,12 +17,13 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.insert(0, os.path.abspath("../../.."))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 # -- Project information -----------------------------------------------------
 
-project = 'YLearn'
-copyright = '2022, DataCanvasLab'
-author = 'DataCanvasLab'
+project = "YLearn"
+copyright = "2022, DataCanvasLab"
+author = "DataCanvasLab"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,12 +32,12 @@ author = 'DataCanvasLab'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,12 +50,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # import ylearn
 # print("cwd: ")
@@ -63,7 +64,3 @@ html_static_path = ['_static']
 # # print(build_cmd)
 
 # os.system(build_cmd)
-
-
-
-
