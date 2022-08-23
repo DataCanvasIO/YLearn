a2d068c95a64424764660f79eef172da3cdc9db2
Haifeng Wu <oaksharks@163.com>
2022-08-23 18:28:05 +0800

    move tree plot to _export.py

--- ylearn/effect_interpreter/ce_interpreter.py
+++ ylearn/effect_interpreter/ce_interpreter.py
@@ -8,255 +8,11 @@ import re
 import numpy as np
 import pandas as pd
 
-from sklearn.tree import plot_tree
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.tree._export import _MPLTreeExporter
-from sklearn.tree._reingold_tilford import buchheim
 
 
+from ylearn.effect_interpreter._export import _CateTreeExporter
 from ylearn.estimator_model.utils import convert2array
-
-
-class _CateTreeExporter(_MPLTreeExporter):
-
-    def __init__(self, node_dict, include_uncertainty=False, uncertainty_level=0.1,
-                 *args, treatment_names=None, **kwargs):
-        self.node_dict = node_dict
-        self.include_uncertainty = include_uncertainty
-        self.uncertainty_level = uncertainty_level
-        self.treatment_names = treatment_names
-        super().__init__(*args, **kwargs)
-
-    def get_color(self, value):
-        # Find the appropriate color & intensity for a node
-        if self.colors["bounds"] is None:
-            # Classification tree
-            color = list(self.colors["rgb"][np.argmax(value)])
-            sorted_values = sorted(value, reverse=True)
-            if len(sorted_values) == 1:
-                alpha = 0
-            else:
-                alpha = (sorted_values[0] - sorted_values[1]) / (1 - sorted_values[1])
-        else:
-            # Regression tree or multi-output
-            color = list(self.colors["rgb"][0])
-            alpha = (value - self.colors["bounds"][0]) / (
-                self.colors["bounds"][1] - self.colors["bounds"][0]
-            )
-        # unpack numpy scalars
-        alpha = float(alpha)
-        # compute the color as alpha against white
-        color = [int(round(alpha * c + (1 - alpha) * 255, 0)) for c in color]
-        # Return html color code in #RRGGBB format
-        return "#%02x%02x%02x" % tuple(color)
-
-    def get_fill_color(self, tree, node_id):
-
-        # Fetch appropriate color for node
-        if 'rgb' not in self.colors:
-            # red for negative, green for positive
-            self.colors['rgb'] = [(233, 150, 60), (6, 42, 220)]
-
-        # in multi-target use mean of targets
-        tree_min = np.min(np.mean(tree.value, axis=1)) - 1e-12
-        tree_max = np.max(np.mean(tree.value, axis=1)) + 1e-12
-
-        node_val = np.mean(tree.value[node_id])
-
-        if node_val > 0:
-            value = [max(0, tree_min) / tree_max, node_val / tree_max]
-        elif node_val < 0:
-            value = [node_val / tree_min, min(0, tree_max) / tree_min]
-        else:
-            value = [0, 0]
-
-        return self.get_color(value)
-
-    def export(self, decision_tree, ax=None):
-        import matplotlib.pyplot as plt
-        from matplotlib.text import Annotation
-
-        if ax is None:
-            ax = plt.gca()
-        ax.clear()
-        ax.set_axis_off()
-        my_tree = self._make_tree(0, decision_tree.tree_, decision_tree.criterion)
-        draw_tree = buchheim(my_tree)
-
-        # important to make sure we're still
-        # inside the axis after drawing the box
-        # this makes sense because the width of a box
-        # is about the same as the distance between boxes
-        max_x, max_y = draw_tree.max_extents() + 1
-        ax_width = ax.get_window_extent().width
-        ax_height = ax.get_window_extent().height
-
-        scale_x = ax_width / max_x
-        scale_y = ax_height / max_y
-        self.recurse(draw_tree, decision_tree.tree_, ax, max_x, max_y, text_pos=0)
-
-        anns = [ann for ann in ax.get_children() if isinstance(ann, Annotation)]
-
-        # update sizes of all bboxes
-        renderer = ax.figure.canvas.get_renderer()
-
-        for ann in anns:
-            ann.update_bbox_position_size(renderer)
-
-        if self.fontsize is None:
-            # get figure to data transform
-            # adjust fontsize to avoid overlap
-            # get max box width and height
-            extents = [ann.get_bbox_patch().get_window_extent() for ann in anns]
-            max_width = max([extent.width for extent in extents])
-            max_height = max([extent.height for extent in extents])
-            # width should be around scale_x in axis coordinates
-            size = anns[0].get_fontsize() * min(
-                scale_x / max_width, scale_y / max_height
-            )
-            for ann in anns:
-                ann.set_fontsize(size)
-
-        return anns
-
-    def recurse(self, node, tree, ax, max_x, max_y, text_pos,  depth=0):
-        import matplotlib.pyplot as plt
-
-        kwargs = dict(
-            bbox=self.bbox_args.copy(),
-            ha="center",
-            va="center",
-            zorder=100 - 10 * depth,
-            xycoords="axes fraction",
-            arrowprops=self.arrow_args.copy(),
-        )
-        kwargs["arrowprops"]["edgecolor"] = plt.rcParams["text.color"]
-
-        if self.fontsize is not None:
-            kwargs["fontsize"] = self.fontsize
-
-        # offset things by .5 to center them in plot
-        xy = ((node.x + 0.5) / max_x, (max_y - node.y - 0.5) / max_y)
-
-        if self.max_depth is None or depth <= self.max_depth:
-            if self.filled:
-                kwargs["bbox"]["fc"] = self.get_fill_color(tree, node.tree.node_id)
-            else:
-                kwargs["bbox"]["fc"] = ax.get_facecolor()
-
-            if node.parent is None:
-                # root
-                ax.annotate(node.tree.label, xy, **kwargs)
-            else:
-                xy_parent = (
-                    (node.parent.x + 0.5) / max_x,
-                    (max_y - node.parent.y - 0.5) / max_y,
-                )
-                ax.annotate(node.tree.label, xy_parent, xy, **kwargs)
-
-                text_pos_mapping = {
-                    1: ('yes', 'right', -0.015),
-                    -1: ('no', 'left', 0.015)
-                    # 0: ('center', 0, '')
-                }
-
-                if text_pos in [1, -1]:
-                    text_pos_config = text_pos_mapping[text_pos]
-                    ax.text((xy_parent[0] - xy[0])/2 + xy[0] + text_pos_config[2],
-                            (xy_parent[1] - xy[1])/2 + xy[1],
-                            text_pos_config[0], va="center", ha=text_pos_config[1], rotation=0)
-
-            n_children = len(node.children)
-            for i, child in enumerate(node.children):
-                if i == 0:
-                    next_text_pos = 1
-                elif i == n_children - 1:
-                    next_text_pos = -1
-                else:
-                    next_text_pos = 0
-                self.recurse(child, tree, ax, max_x, max_y, text_pos=next_text_pos, depth=depth + 1)
-
-        else:
-            xy_parent = (
-                (node.parent.x + 0.5) / max_x,
-                (max_y - node.parent.y - 0.5) / max_y,
-            )
-            kwargs["bbox"]["fc"] = "grey"
-            ax.annotate("\n  (...)  \n", xy_parent, xy, **kwargs)
-
-    def node_replacement_text(self, tree, node_id, criterion):
-        # Write node mean CATE
-        node_info = self.node_dict[node_id]
-        node_string = 'CATE mean' + self.characters[4]
-        value_text = ""
-        mean = node_info['mean']
-        if hasattr(mean, 'shape') and (len(mean.shape) > 0):
-            if len(mean.shape) == 1:
-                for i in range(mean.shape[0]):
-                    value_text += "{}".format(np.around(mean[i], self.precision))
-                    if 'ci' in node_info:
-                        value_text += " ({}, {})".format(np.around(node_info['ci'][0][i], self.precision),
-                                                         np.around(node_info['ci'][1][i], self.precision))
-                    if i != mean.shape[0] - 1:
-                        value_text += ", "
-                value_text += self.characters[4]
-            elif len(mean.shape) == 2:
-                for i in range(mean.shape[0]):
-                    for j in range(mean.shape[1]):
-                        value_text += "{}".format(np.around(mean[i, j], self.precision))
-                        if 'ci' in node_info:
-                            value_text += " ({}, {})".format(np.around(node_info['ci'][0][i, j], self.precision),
-                                                             np.around(node_info['ci'][1][i, j], self.precision))
-                        if j != mean.shape[1] - 1:
-                            value_text += ", "
-                    value_text += self.characters[4]
-            else:
-                raise ValueError("can only handle up to 2d values")
-        else:
-            value_text += "{}".format(np.around(mean, self.precision))
-            if 'ci' in node_info:
-                value_text += " ({}, {})".format(np.around(node_info['ci'][0], self.precision),
-                                                 np.around(node_info['ci'][1], self.precision))
-            value_text += self.characters[4]
-        node_string += value_text
-
-        # Write node std of CATE
-        node_string += "CATE std" + self.characters[4]
-        std = node_info['std']
-        value_text = ""
-        if hasattr(std, 'shape') and (len(std.shape) > 0):
-            if len(std.shape) == 1:
-                for i in range(std.shape[0]):
-                    value_text += "{}".format(np.around(std[i], self.precision))
-                    if i != std.shape[0] - 1:
-                        value_text += ", "
-            elif len(std.shape) == 2:
-                for i in range(std.shape[0]):
-                    for j in range(std.shape[1]):
-                        value_text += "{}".format(np.around(std[i, j], self.precision))
-                        if j != std.shape[1] - 1:
-                            value_text += ", "
-                    if i != std.shape[0] - 1:
-                        value_text += self.characters[4]
-            else:
-                raise ValueError("can only handle up to 2d values")
-        else:
-            value_text += "{}".format(np.around(std, self.precision))
-        node_string += value_text
-        return node_string
-
-    def node_to_str(self, tree, node_id, criterion):
-        text = super().node_to_str(tree, node_id, criterion)
-        replacement = self.node_replacement_text(tree, node_id, criterion)
-        if replacement is not None:
-            # HACK: it's not optimal to use a regex like this, but the base class's node_to_str doesn't expose any
-            #       clean way of achieving this
-            text = re.sub("value = .*(?=" + re.escape(self.characters[5]) + ")",
-                          # make sure we don't accidentally escape anything in the substitution
-                          replacement.replace('\\', '\\\\'),
-                          text,
-                          flags=re.S)
-        return text
 
 
 class CEInterpreter:
@@ -419,7 +175,6 @@ class CEInterpreter:
         node_dict = {}
         for node_id in range(paths.shape[1]):
             mask = paths.getcol(node_id).toarray().flatten().astype(bool)
-            # Xsub = v[mask]
             cate_node = causal_effect[mask]
             node_dict[node_id] = {'mean': np.mean(cate_node, axis=0), 'std': np.std(cate_node, axis=0)}
 
@@ -598,7 +353,6 @@ class CEInterpreter:
             feature_names = self.covariate
 
         exporter = _CateTreeExporter(
-            self.node_dict_,
             max_depth=max_depth,
             feature_names=feature_names,
             class_names=class_names,
@@ -611,4 +365,5 @@ class CEInterpreter:
             precision=precision,
             fontsize=fontsize,
         )
-        return exporter.export(self._tree_model, ax=ax)
+
+        return exporter.export(self._tree_model, self.node_dict_, ax=ax)
