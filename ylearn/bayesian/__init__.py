4872fba6d49ab0ddfd3a2a39c2a20e257269b390
lixfz <lixf@sovon.net>
2023-06-02 10:54:52 +0800

    Fix typo

--- ylearn/bayesian/__init__.py
+++ ylearn/bayesian/__init__.py
@@ -2,6 +2,6 @@ from ._dag import DAG, DiGraph
 
 try:
     from ._data import DataLoader
-    from ._network import BayesianNetwork, SviBayesianNetwork, McmcBayesianNetwork
+    from ._network import BayesianNetwork, SviBayesianNetwork
 except ImportError:
     pass
